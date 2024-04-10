56911230907062c1aa8944d6471a7dc57f27401b
Marco <empleomarcotapia@gmail.com>
2024-04-10 08:21:22 -0700

    Initial Commit

---
+++ Contador/ContadorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContadorApp.swift
+//  Contador
+//
+//  Created by Marco on 10/4/24.
+//
+
+import SwiftUI
+
+@main
+struct ContadorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
